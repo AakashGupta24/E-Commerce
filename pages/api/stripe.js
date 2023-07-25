8b17355d00276cafeaf7bfdab8551a9177eb1830
Aakash Gupta <97354708+AakashGupta24@users.noreply.github.com>
2023-07-25 20:33:28 +0530

    all done

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,39 +1,28 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-
-            const params = {
-                submit_type : 'pay',
-                mode:'payment',
-                payment_method_types:['card'],
-                billing_address_collections:'auto',
-                shipping_options:[
-                    {shipping_rate:'shr_1N9lHsSAEBi4CyZLVkkPXgYH'},
-                    {shipping_rate:'shr_1N9lJDSAEBi4CyZLK5UUNXj0'},
-
-                ],
-                line_items: [
-                    {
-                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                        price: '{{PRICE_ID}}',
-                        quantity: 1,
-                    },
-                ],
-                mode: 'payment',
-                success_url: `${req.headers.origin}/?success=true`,
-                cancel_url: `${req.headers.origin}/?canceled=true`,
-
-            }
-            // Create Checkout Sessions from body params.
-            const session = await stripe.checkout.sessions.create();
-            res.redirect(303, session.url);
-        } catch (err) {
-            res.status(err.statusCode || 500).json(err.message);
-        }
-    } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+  if (req.method === 'POST') {
+    try {
+        console.log(req.body[0]);
+        // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: '{{PRICE_Icdkcsncnkcnjc nkc xnj cnjc D}}',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
     }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 }
